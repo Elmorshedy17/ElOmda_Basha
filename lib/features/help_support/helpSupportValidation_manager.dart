cf953419b746624e07e0ed51721ad2a79fb82c1a
Mohamed Nabil <morooo.nabil17@gmail.com>
2020-10-18 11:08:00 +0200

    change password validation & change email validation & help and support validation

--- lib/features/help_support/helpSupportValidation_manager.dart
+++ lib/features/help_support/helpSupportValidation_manager.dart
@@ -1,32 +1,46 @@
 import 'dart:async';
-
 import 'package:momentoo/shared/helper/manager.dart';
 import 'package:momentoo/shared/validation/validation.dart';
 import 'package:rxdart/rxdart.dart';
 
 class HelpSupportValidationManager with Validation implements Manager {
-  // final emailSubject = BehaviorSubject<String>();
-  // Stream<String> get email$ => emailSubject.stream.transform(validateEmail);
-  // Sink<String> get inEmail => emailSubject.sink;
+  final emailSubject = BehaviorSubject<String>();
+  Stream<String> get email$ => emailSubject.stream.transform(validateEmail);
+  Sink<String> get inEmail => emailSubject.sink;
 
-  // final passwordSubject = BehaviorSubject<String>();
-  // Stream<String> get password$ =>
-  //     passwordSubject.stream.transform(validateField);
-  // Sink<String> get inPassword => passwordSubject.sink;
 
-  final _checkBoxSubject = BehaviorSubject<bool>.seeded(true);
-  Stream<bool> get checkBoxValue$ => _checkBoxSubject.stream;
-  void setCheckBoxValue(bool value) {
-    _checkBoxSubject.sink.add(value);
-  }
 
-  // Stream<bool> get isFormValid$ =>
-  //     Rx.combineLatest([email$, password$], (values) => true);
+  final firstNameSubject = BehaviorSubject<String>();
+  Stream<String> get firstName$ =>
+      firstNameSubject.stream.transform(validateField);
+  Sink<String> get inFirstName => firstNameSubject.sink;
+
+  final msgSubject = BehaviorSubject<String>();
+  Stream<String> get msg$ => msgSubject.stream.transform(validateField);
+  Sink<String> get inMsg => msgSubject.sink;
+
+
+
+
+  final phoneSubject = BehaviorSubject<String>();
+  Stream<String> get phone$ =>
+      phoneSubject.stream.transform(validatePhoneField);
+  Sink<String> get inPhone => phoneSubject.sink;
+
+
+
+  Stream<bool> get isFormValid$ => Rx.combineLatest([
+    email$,
+    firstName$,
+    phone$,
+    msg$
+  ], (values) => true);
 
   @override
   dispose() {
-    // emailSubject.close();
-    // passwordSubject.close();
-    _checkBoxSubject.close();
+    emailSubject.close();
+    msgSubject.close();
+    firstNameSubject.close();
+    phoneSubject.close();
   }
 }
